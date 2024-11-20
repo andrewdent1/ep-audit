7392e4a377ab9e552f47ad2dfcbe2d5c90c7b788
Andrew Dent <dentdrew@gmail.com>
2024-11-20 10:57:21 -0500

    link building

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+        './index.html',        // If you have an index.html in the root
+        './src/**/*.{html,js}', // For any HTML or JS files in the src folder
+        './public/**/*.{html,js}', // If you have public folder where HTML is located
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
